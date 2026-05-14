0921ff0458bd084516e24eb37ca252d32766eae3
matheusydev <matheusylanaraujo@gmail.com>
2026-05-14 10:31:09 -0300

    refactor: removendo módulo de pacientes e adicionando consultas

--- consultas/apps.py
+++ consultas/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PacientesConfig(AppConfig):
+class ConsultasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pacientes'
+    name = 'consultas'
