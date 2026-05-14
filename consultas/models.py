8e6b3228135c34f0ca729c69c8673c7d0bb292f9
matheusydev <matheusylanaraujo@gmail.com>
2026-05-14 17:47:42 -0300

    WIP: Criação dos endpoints em andamento. Tabela Consulta criada

--- consultas/models.py
+++ consultas/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Consulta(models.Model):
+    data = models.DateTimeField()
+    nome_medico = models.CharField(max_length=100)
+    especialidade = models.CharField(max_length=50)
+    localizacao = models.CharField(max_length=100)
+
+
+
+# export type Consulta = {
+#   id: string;
+#   data: Date;
+#   medico: string;
+#   especialidade: string;
+#   localizacao: string
+# };
