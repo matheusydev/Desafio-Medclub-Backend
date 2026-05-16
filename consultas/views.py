ea65f111dce820ff4c4a64d76c1f8810a373a0f3
Matheusydev <matheusylanaraujo@gmail.com>
2026-05-16 09:29:23 -0300

    feat: implementado o fluxo de criação de consultas

--- consultas/views.py
+++ consultas/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Consulta
 
 # Create your views here.
 def criar_consulta(request):
-    return render(request, 'criar_consulta.html')
+    if request.method == 'GET':
+        return render(request, 'criar_consulta.html')
+    elif request.method == 'POST':
+        nome_medico = request.POST.get('nome_medico')
+        data = request.POST.get('data')
+        especialidade = request.POST.get('especialidade')
+        localizacao = request.POST.get('localizacao')
+
+        consulta = Consulta(
+            nome_medico = nome_medico,
+            data = data,
+            especialidade = especialidade,
+            localizacao = localizacao,
+        )
+        
+        consulta.save()
+
+        return redirect('criar_consulta')
+        
