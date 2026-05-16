1ed8fcc7f146b5e79300e2d15838e9397b50b73e
Matheusydev <matheusylanaraujo@gmail.com>
2026-05-16 09:58:51 -0300

    feat: configuração das rotas da API com DefaultRouter e prefixo api/

--- consultas/urls.py
+++ consultas/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from .views import ConsultaViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'consultas', ConsultaViewSet)
 
 urlpatterns = [
-    path('', views.criar_consulta, name='criar_consulta'),
+    path('', include(router.urls)),
 ]
