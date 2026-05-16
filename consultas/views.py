1177262f71e5a0ee6701286881ae1dcfb2dfd70a
Matheusydev <matheusylanaraujo@gmail.com>
2026-05-16 09:48:33 -0300

    feat: adicionado a ConsultaViewSet com ModelViewSet para CRUD

--- consultas/views.py
+++ consultas/views.py
@@ -1,24 +1,9 @@
-from django.shortcuts import render, redirect
 from .models import Consulta
+from rest_framework import viewsets
+from .serializers import ConsultaSerializer
 
 # Create your views here.
-def criar_consulta(request):
-    if request.method == 'GET':
-        return render(request, 'criar_consulta.html')
-    elif request.method == 'POST':
-        nome_medico = request.POST.get('nome_medico')
-        data = request.POST.get('data')
-        especialidade = request.POST.get('especialidade')
-        localizacao = request.POST.get('localizacao')
-
-        consulta = Consulta(
-            nome_medico = nome_medico,
-            data = data,
-            especialidade = especialidade,
-            localizacao = localizacao,
-        )
-        
-        consulta.save()
-
-        return redirect('criar_consulta')
+class ConsultaViewSet(viewsets.ModelViewSet):
+    queryset = Consulta.objects.all()
+    serializer_class = ConsultaSerializer
         
