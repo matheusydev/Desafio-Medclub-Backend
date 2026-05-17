beeef5da049996d58b385c6cfd48210d3de8af15
Matheusydev <matheusylanaraujo@gmail.com>
2026-05-17 12:04:54 -0300

    config: adiciona variáveis de ambiente com python-decouple

--- Desafio_Medclub_Backend/settings.py
+++ Desafio_Medclub_Backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1t*uty^1uut&p#oo_6bak$42o2giqd$^s*lbdml9+*mlfz!j8o'
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', cast=bool)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
