5bfd237caaa5c5043e6dbd263017c7685e7ef921
Matheusydev <matheusylanaraujo@gmail.com>
2026-05-17 23:04:48 -0300

    feat: registrar o modelo Consulta no painel administrativo do Django

--- consultas/admin.py
+++ consultas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Consulta
 
 # Register your models here.
+
+@admin.register(Consulta)
+class ConsultaAdmin(admin.ModelAdmin):
+    list_display = ['nome_medico','data', 'localizacao']
+    search_fields = ['nome_medico','data']
