e77dc5a6c13cebfeadc27623e2ccc1380973766e
Matheusydev <matheusylanaraujo@gmail.com>
2026-05-17 22:44:36 -0300

    feat: adiciona filtros por nome_do_médico e data usando django-filter e povoação do banco

--- consultas/views.py
+++ consultas/views.py
@@ -2,11 +2,14 @@ from .models import Consulta
 from rest_framework import viewsets
 from .serializers import ConsultaSerializer
 from drf_spectacular.utils import extend_schema
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 class ConsultaViewSet(viewsets.ModelViewSet):
     queryset = Consulta.objects.all()
     serializer_class = ConsultaSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['nome_medico', 'data']
 
     @extend_schema(summary="Lista todas as consultas")
     def list(self, request, *args, **kwargs):
