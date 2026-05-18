2aa039f78d3c0e7bcbcb4dc92d277828bb9c5164
Matheusydev <matheusylanaraujo@gmail.com>
2026-05-17 22:49:46 -0300

    feat: adiciona validação de data no ConsultaSerializer

--- consultas/serializers.py
+++ consultas/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Consulta
+from datetime import date
 
 class ConsultaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Consulta
         fields = '__all__'
+
+    def validate_data(self, value):
+        if value < date.today():
+            raise serializers.ValidationError("A data de consulta não pode ser no passado.")
+        return value
